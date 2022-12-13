831bda8a65507b4b123aefcdb97c377bce43bc76
Savitarium <grod@savitarium.com>
2022-12-13 18:39:57 +0100

    Add find correct article section

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@
         console.log ('links:', links);
     });*/
     const titleClickHandler = function (event){
+        event.preventDefault();
         const clickedElement = this;
         console.log('clickedElement (with plus): ' + clickedElement);
         /* [DONE] remove class 'active' from all article links  */
@@ -13,7 +14,7 @@
         for(let activeLink of activeLinks){
             activeLink.classList.remove('active');
         }
-        /* [IN PROGRESS] add class 'active' to the clicked link */
+        /* [DONE] add class 'active' to the clicked link */
         clickedElement.classList.add('active');
         /* [DONE] remove class 'active' from all articles */
         const activeArticles = document.querySelectorAll('.posts .active');
@@ -21,10 +22,12 @@
         for(let activeArticle of activeArticles){
             activeArticle.classList.remove('active');
         }
-        /* get 'href' attribute from the clicked link */
-
-        /* find the correct article using the selector (value of 'href' attribute) */
-
+        /* [DONE] get 'href' attribute from the clicked link */
+        const articleSelector = clickedElement.getAttribute('href');
+        console.log('articleSelector: ' + articleSelector);
+        /* [DONE] find the correct article using the selector (value of 'href' attribute) */
+        const targetArticle = document.querySelector(articleSelector);
+        console.log(targetArticle);
         /* add class 'active' to the correct article */
     }
 
